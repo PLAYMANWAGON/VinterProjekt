f74f44bc9855cbc5f76cdf2d203222646b65c1d9
PLAYMANWAGON <aironas.keturka@elev.ga.ntig.se>
2021-11-22 12:20:58 +0100

    Game WIndow skapats

---
+++ VinterProjekt/Character.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace VinterProjekt
+{
+    public class Character
+    {
+        private string name;
+
+        private int hp;
+        private int mp;
+        private int sp;
+
+        public int x;
+        public int y;
+    }
+}

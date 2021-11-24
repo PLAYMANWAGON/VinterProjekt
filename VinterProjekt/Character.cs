5ffde5332df33824d59e62e64fe5dc4bcc7f3d97
PLAYMANWAGON <aironas.keturka@elev.ga.ntig.se>
2021-11-24 15:03:00 +0100

    DrawChar och UpdateMove implementerats

--- VinterProjekt/Character.cs
+++ VinterProjekt/Character.cs
@@ -1,16 +1,59 @@
 using System;
+using Raylib_cs;
 
-namespace VinterProjekt
+public class Character
 {
-    public class Character
+    private string name = "Jorgan";
+
+    private int hp;     //Health Points
+    private int mp;     //Magic Points
+    private int sp;     //Shield Points
+
+    private float speed = 200;
+
+    public float x = 10;
+    public float y = 10;
+
+    public void DrawCharacter()
     {
-        private string name;
+        Raylib.DrawRectangle((int)x,(int)y,100,100,Color.GREEN);
+    }
 
-        private int hp;
-        private int mp;
-        private int sp;
+    public void UpdateCharacter()
+    {
 
-        public int x;
-        public int y;
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_X))
+        {
+            speed = 400;
+        }
+        else
+        {
+            speed = 200;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+        {
+            y -= speed * Raylib.GetFrameTime();
+
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+        {
+            y += speed * Raylib.GetFrameTime();
+
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            x -= speed * Raylib.GetFrameTime();
+
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            x += speed * Raylib.GetFrameTime();
+
+        }
+
     }
 }
--- VinterProjekt/Program.cs
+++ VinterProjekt/Program.cs
@@ -1,2 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using Raylib_cs;
 
+Console.WriteLine("Hello, World!");
+
+Character player = new Character();
+
+Raylib.InitWindow(800, 600, "CHOBLAN GAME");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.WHITE);
+
+    player.DrawCharacter();
+    player.UpdateCharacter();
+
+    Raylib.EndDrawing();
+}
